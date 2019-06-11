755bfcce7964b60d9f0d63969b7340007996423b
Gerard Otero <jerry.om.95@gmail.com>
2019-06-11 20:13:23 +0200

    Deleted big dataset and added a smaller new one, added first web app things + modified a little the workflow to work alone and on shiny

--- workflow.R
+++ workflow.R
@@ -13,15 +13,6 @@ if (!'mixOmics' %in% installed.packages()){
 
 library(mixOmics)
 
-load("dataframes.RData")
-
-####### PREPARATION #######
-
-mas5_matrix = as.matrix.data.frame(mas5)
-
-ALS = which(classes == "ALS")
-noALS = which(classes != "ALS")
-
 ####### FISHER #######
 
 # to calculate Fisher score of each feature: 
@@ -30,23 +21,28 @@ fisher = function(gene, pos, neg) {
     	return(score)
 }
 
-apply_fisher = function(dataset, positive, negative, features_to_keep = 5000){
+apply_fisher = function(dataset, positive, negative, features_to_keep = 5000, debug = TRUE){
 
+  output = NULL
+  
 	scores = apply(dataset, 2, fisher, positive, negative)
 
-    	print(length(scores))
+	print(length(scores))
 
-    	stopifnot(length(scores) == dim(dataset)[2])
+	stopifnot(length(scores) == dim(dataset)[2])
 
-    	sorted_scores = sort(scores, decreasing=TRUE, index.return=TRUE)
-    	plot(sorted_scores$x)
+	sorted_scores = sort(scores, decreasing=TRUE, index.return=TRUE)
+	if (debug){
+	  plot(sorted_scores$x)
+	  
+	  dev.new()
+	} else {
+	  output$to_plot = sorted_scores$x
+	}
+	# We keep the best x features according to fda, 5000 by default but the user should be able to input it.
+	output$data = dataset[, sort(sorted_scores$ix[0:features_to_keep])]
 
-	dev.new()
-
-    	# We keep the best x features according to fda, 5000 by default but the user should be able to input it.
-    	after_fisher = dataset[, sort(sorted_scores$ix[0:features_to_keep])]
-
-	return(after_fisher)
+	return(output)
 }
 
 
@@ -55,14 +51,14 @@ apply_fisher = function(dataset, positive, negative, features_to_keep = 5000){
 set.seed(1337)
 
 # Applies ReliefF. For now, only ReliefFequalK, but I plan on using exp too and adding a parameter to specify it
-apply_relieff = function(dataset, classes, features_to_keep = 500, iterations = 0){
+apply_relieff = function(dataset, classes, features_to_keep = 500, iterations = 0, estimator = 'ReliefFexpRank', debug = TRUE){
 	
 	stopifnot(length(classes) == dim(dataset)[1])
 
 	df_ = as.data.frame(dataset)
 	df = cbind(df_, class=classes)
 
-	reliefF_attrs = attrEval('class', df, estimator='ReliefFexpRank', ReliefIterations=iterations)
+	reliefF_attrs = attrEval('class', df, estimator=estimator, ReliefIterations=iterations)
 
 	sorted_attrs = sort(reliefF_attrs, decreasing=TRUE, index.return=TRUE)
 
@@ -79,7 +75,7 @@ apply_relieff = function(dataset, classes, features_to_keep = 500, iterations = 
 
 ####### PCA #######
 
-apply_pca = function(dataset, classes, components = 10){
+apply_pca = function(dataset, classes, components = 10, debug = TRUE){
 	pca_plot = pca(dataset, ncomp=components)
 	plot(pca_plot)
 	dev.new()
@@ -90,7 +86,7 @@ apply_pca = function(dataset, classes, components = 10){
 
 ####### PLS-DA #######
 
-apply_plsda = function(dataset, classes, components = 10, cv_folds = 5, cv_repeats = 10){
+apply_plsda = function(dataset, classes, components = 10, cv_folds = 5, cv_repeats = 10, debug = TRUE){
 	plsda_data = plsda(dataset, classes, ncomp=components, scale=FALSE)
 
   perf_plsda_data = perf(plsda_data, validation = 'Mfold', folds = cv_folds, progressBar = TRUE, nrepeat = cv_repeats)
@@ -140,19 +136,19 @@ apply_plsda = function(dataset, classes, components = 10, cv_folds = 5, cv_repea
 }
 
 ####### MAIN FUNCTION TO EXECUTE #######
-apply_workflow = function(dataset, classes, fisher_variables = 5000, relieff_variables = 500, pca_components = 10, cv_folds = 5, cv_repeats = 10){
+apply_workflow = function(dataset, classes, fisher_variables = 5000, relieff_variables = 500, pca_components = 10, cv_folds = 5, cv_repeats = 10, debug=TRUE){
 	# Process initial data
 	data_matrix = as.matrix.data.frame(dataset)
 	positives = which(classes == classes[1])
 	negatives = which(classes != classes[1])
 
-	after_fisher = apply_fisher(data_matrix, positives, negatives)
+	after_fisher = apply_fisher(data_matrix, positives, negatives, debug = debug)
 
-	after_relieff = apply_relieff(after_fisher, classes)
+	after_relieff = apply_relieff(after_fisher, classes, debug = debug)
 
-	apply_pca(after_relieff, classes)
+	apply_pca(after_relieff, classes, debug = debug)
 	
-	final_results = apply_plsda(after_relieff, classes)
+	final_results = apply_plsda(after_relieff, classes, debug = debug)
 	
 	return(final_results)
 }
