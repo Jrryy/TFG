7f539cadaa0429bd21d2c10cb3e7a281768b6f7f
Gerard Otero <jerry.om.95@gmail.com>
2019-07-08 19:48:18 +0200

    Added vertical indicator line to fisher data and completed preprocessing of the data

--- TFG_workflow/app.R
+++ TFG_workflow/app.R
@@ -23,19 +23,21 @@ ui <- fluidPage(
          fileInput('dataset_file',
                    'Dataset file'),
          tags$hr(),
+         #### Fisher ####
          tags$h3(paste0('Parameters for Fisher scoring')),
-         numericInput('fisher_vars', 'Amount of variables to keep:', min = 0, value = 5000),
+         numericInput('fisher_vars', 'Amount of variables to keep:', min = 1, value = 5000),
+         actionButton('submit_fisher', 'Apply Fisher filter'),
          
          tags$hr(),
-         
+         #### ReliefF ####
          tags$h3(paste0('Parameters for ReliefF')),
          checkboxInput('use_relieff', 'Perform a ReliefF step?', value = TRUE),
-         numericInput('relieff_vars', 'Amount of variables to keep:', min = 0, value = 1000),
+         numericInput('relieff_vars', 'Amount of variables to keep:', min = 1, value = 1000),
          selectInput('relieff_iters', 'Amount of iterations to perform:', c('Dataset size' = 0, 'ln(Dataset size)' = -1, 'sqrt(Dataset size)' = -2), selected = 0),
          selectInput('relieff_method', 'ReliefF algorithm to calculate the scores:', c('ReliefFequalK', 'ReliefFexpRank'), selected = 'ReliefFequalK'),
          
          tags$hr(),
-         
+         #### PCA ####
          tags$h3(paste0('Parameters for sPLS-DA')),
          numericInput('components', 'Amount of PCA components to extract', value = 10, min = 2),
          numericInput('cv_folds', 'Amount of cross validation folds to perform', value = 5, min = 1),
@@ -48,9 +50,9 @@ ui <- fluidPage(
       
       # Show a plot of the generated distribution
       mainPanel(
-         plotOutput('fisher_plot'),
+         span(textOutput('error_text'), style='color:red'),
          textOutput('waiting_text'),
-         textOutput('error_text')
+         plotOutput('fisher_plot')
       )
    )
 )
@@ -58,38 +60,71 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
   options(shiny.maxRequestSize = 100*1024^2)
+  input_error = TRUE
   
-  observeEvent(input$submit, {
-    output$waiting_text = renderText('Please wait for the calculations to finish...')
+  #### LOAD THE DATA AND PROCESS THE FIRST DATA ####
+  observeEvent(input$dataset_file, {
+    output$waiting_text = renderText('Please wait for the first calculations to finish...')
     
     dataset_file = input$dataset_file
-    dataset = load(dataset_file$datapath)
-    if (!('classes' %in% dataset)){
-      output$waiting_text = NULL
-      output$error_text = renderText('ERROR: There is no classes array in the dataset! Please upload a valid dataset.')
-      print(length(dataset))
-    } else if (length(dataset) != 2){
-      output$waiting_text = NULL
-      output$error_text = renderText('ERROR: This dataset includes a wrong amount of data! Please upload a valid dataset.')
+    # First let's obtain the input data and check if it's well introduced
+    if (endsWith(dataset_file$name, ".csv")){
+      csv_data = read.csv(file = dataset_file$datapath, header = TRUE, as.is = TRUE)
+      if(!('classes' %in% colnames(csv_data))){
+        output$waiting_text = NULL
+        output$error_text = renderText('ERROR: There is no classes column in the dataset! Please upload a valid dataset.')
+      } else{
+        input_error = FALSE
+        output$error_text = NULL
+        classes = csv_data$classes
+        classes_position = match('classes', colnames(csv_data))
+        data_matrix = as.matrix.data.frame(csv_data[-classes_position])
+        positives = which(classes == classes[1])
+        negatives = which(classes != classes[1])
+      }
+    } else if (endsWith(dataset_file$name, ".RData")){
+      dataset = load(dataset_file$datapath)
+      if(!('classes' %in% dataset)){
+        output$waiting_text = NULL
+        output$error_text = renderText('ERROR: There is no classes array in the dataset! Please upload a valid dataset.')
+      } else if (length(dataset) != 2){
+        output$waiting_text = NULL
+        output$error_text = renderText('ERROR: This dataset includes a wrong amount of data! Please upload a valid dataset.')
+      } else {
+        input_error = FALSE
+        output$error_text = NULL
+        data_matrix = as.matrix.data.frame(get(dataset[3 - match('classes', dataset)]))
+        positives = which(classes == classes[1])
+        negatives = which(classes != classes[1])
+      }
     } else {
-      data_matrix = as.matrix.data.frame(get(dataset[3 - match('classes', dataset)]))
-      positives = which(classes == classes[1])
-      negatives = which(classes != classes[1])
-      
+      output$waiting_text = NULL
+      output$error_text = renderText('ERROR: The file you uploaded is not valid. Make sure you upload a csv or RData file.')
+    }
+    
+    if (!(input_error)){
       after_fisher = apply_fisher(data_matrix, positives, negatives, features_to_keep = input$fisher_vars, debug = FALSE)
-      
-      output$fisher_plot = renderPlot(plot(after_fisher$to_plot, ylab = 'Score'))
+      output$fisher_plot = renderPlot({
+        plot(after_fisher$to_plot, ylab = 'Score')
+        abline(v = input$fisher_vars, col = 'red', lwd = 2)
+        })
     }
   })
-   
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
-   })
+  
+  #### TO CHANGE THE INDICATOR LINE IN THE FISHER CHART ####
+  observeEvent(input$fisher_vars, {
+    if (!(input_error)){
+      if (exists('after_fisher')){
+        output$fisher_plot = renderPlot({
+          plot(after_fisher$to_plot, ylab = 'Score')
+          abline(v = input$fisher_vars, col = 'red', lwd = 2)
+        })
+      }
+    }
+  })
+  
+  
+  
 }
 
 # Run the application 
